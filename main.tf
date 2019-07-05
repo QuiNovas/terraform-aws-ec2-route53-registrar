ab8f434c05834ba753b176ce6839b377c55633be
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-05 14:27:12 +0530

    updated to terraform 12

--- main.tf
+++ main.tf
@@ -4,24 +4,24 @@ resource "random_string" "name_suffix" {
 }
 
 module "ec2_route53_registrar" {
-  dead_letter_arn = "${var.dead_letter_arn}"
+  dead_letter_arn = var.dead_letter_arn
   handler         = "function.handler"
-  kms_key_arn     = "${var.kms_key_arn}"
+  kms_key_arn     = var.kms_key_arn
   l3_object_key   = "quinovas/ec2-route53-registrar/ec2-route53-registrar-0.0.1.zip"
 
-  environment_variables {
-    DNS_NAME_PREFIX    = "${var.dns_name_prefix}"
-    DNS_NAME_SUFFIX    = "${var.dns_name_suffix}"
-    DNS_TTL            = "${var.dns_ttl}"
-    EC2_INSTANCE_TAGS  = "${jsonencode(var.ec2_instance_tags)}"
-    HOSTED_ZONE_ID     = "${var.hosted_zone_id}"
-    MULTI_VALUE_ANSWER = "${var.multi_value_answer}"
+  environment_variables = {
+    DNS_NAME_PREFIX    = var.dns_name_prefix
+    DNS_NAME_SUFFIX    = var.dns_name_suffix
+    DNS_TTL            = var.dns_ttl
+    EC2_INSTANCE_TAGS  = jsonencode(var.ec2_instance_tags)
+    HOSTED_ZONE_ID     = var.hosted_zone_id
+    MULTI_VALUE_ANSWER = var.multi_value_answer
   }
 
   name = "${var.name_prefix}ec2-route53-registrar-${random_string.name_suffix.result}"
 
   policy_arns = [
-    "${aws_iam_policy.ec2_route53_registrar.arn}",
+    aws_iam_policy.ec2_route53_registrar.arn,
     "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
   ]
 
@@ -29,12 +29,12 @@ module "ec2_route53_registrar" {
   runtime           = "python3.7"
   source            = "QuiNovas/lambdalambdalambda/aws"
   timeout           = 300
-  version           = "0.2.0"
+  version           = "3.0.0"
 }
 
 resource "aws_iam_policy" "ec2_route53_registrar" {
   name   = "${var.name_prefix}ec2-route53-registrar--${random_string.name_suffix.result}"
-  policy = "${data.aws_iam_policy_document.ec2_route53_registrar.json}"
+  policy = data.aws_iam_policy_document.ec2_route53_registrar.json
 }
 
 resource "aws_cloudwatch_event_rule" "ec2_route53_registrar" {
@@ -60,19 +60,21 @@ resource "aws_cloudwatch_event_rule" "ec2_route53_registrar" {
 }
 PATTERN
 
+
   name = "${var.name_prefix}ec2-route53-registrar-${random_string.name_suffix.result}"
 }
 
 resource "aws_lambda_permission" "ec2_route53_registrar" {
-  action        = "lambda:InvokeFunction"
-  function_name = "${module.ec2_route53_registrar.name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.ec2_route53_registrar.arn}"
-  statement_id  = "${module.ec2_route53_registrar.name}-execution-permission"
+  action = "lambda:InvokeFunction"
+  function_name = module.ec2_route53_registrar.name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.ec2_route53_registrar.arn
+  statement_id = "${module.ec2_route53_registrar.name}-execution-permission"
 }
 
 resource "aws_cloudwatch_event_target" "ec2_route53_registrar" {
-  depends_on = ["aws_lambda_permission.ec2_route53_registrar"]
-  arn        = "${module.ec2_route53_registrar.arn}"
-  rule       = "${aws_cloudwatch_event_rule.ec2_route53_registrar.name}"
+  depends_on = [aws_lambda_permission.ec2_route53_registrar]
+  arn = module.ec2_route53_registrar.arn
+  rule = aws_cloudwatch_event_rule.ec2_route53_registrar.name
 }
+
--- variables.tf
+++ variables.tf
@@ -1,50 +1,51 @@
 variable "dead_letter_arn" {
   description = "The arn for the SNS topic that handles dead letters"
-  type        = "string"
+  type        = string
 }
 
 variable "dns_name_prefix" {
   default     = ""
   description = "The prefix to put before the record name."
-  type        = "string"
+  type        = string
 }
 
 variable "dns_name_suffix" {
   default     = ""
   description = "The suffix to put after the record name."
-  type        = "string"
+  type        = string
 }
 
 variable "dns_ttl" {
   default     = 300
   description = "The TTL on the created DNS records."
-  type        = "string"
+  type        = number
 }
 
 variable "ec2_instance_tags" {
   default     = {}
   description = "A map of key/value pairs for tags to match. The value is treated as a regular expression for matching purposes."
-  type        = "map"
+  type        = map(string)
 }
 
 variable "hosted_zone_id" {
   description = "The ID that Amazon Route 53 assigned to the hosted zone when you created it. This is the zone that all record sets will be created in."
-  type        = "string"
+  type        = string
 }
 
 variable "kms_key_arn" {
   description = "The arn of the KMS key used to encrypt the environment variables"
-  type        = "string"
+  type        = string
 }
 
 variable "multi_value_answer" {
   default     = false
   description = "If false, the new label will be dns_name_prefix + ec2-dns-label + dns_name_prefix. If true, the new label will be dns_name_prefix + dns_name_prefix. Defaults to false"
-  type        = "string"
+  type        = string
 }
 
 variable "name_prefix" {
   default     = ""
   description = "The prefix to place on all created resources"
-  type        = "string"
+  type        = string
 }
+
